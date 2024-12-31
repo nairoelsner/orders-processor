9a6adb4d31c8207d21167c078dbaa4fc846966f2
Nairo Elsner <nairo.elsner@hotmail.com>
2024-12-31 01:27:16 -0300

    delete consumer

--- nestjs-api/src/main.ts
+++ nestjs-api/src/main.ts
@@ -1,24 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  /*
-  app.connectMicroservice({
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://localhost:5672'],
-      queue: 'order_queue',
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
-  await app.startAllMicroservices();
-*/
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
