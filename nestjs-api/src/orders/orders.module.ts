163b2e99345eb3e7bf061e099fff331ddc28bb17
Nairo Elsner <nairo.elsner@hotmail.com>
2024-12-31 01:28:29 -0300

    update orders module

--- nestjs-api/src/orders/orders.module.ts
+++ nestjs-api/src/orders/orders.module.ts
@@ -1,25 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RabbitmqModule } from 'src/rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [
-    RabbitmqModule,
-    /*
-    ClientsModule.register([
-      {
-        name: 'ORDER_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://localhost:5672'],
-          queue: 'order_queue',
-        },
-      }
-    ]),
-    */
-  ],
+  imports: [RabbitmqModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
