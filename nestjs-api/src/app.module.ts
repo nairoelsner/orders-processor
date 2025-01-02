c310f01268d97943133cf0c92a2c8ff68b82ecb7
Nairo Elsner <nairo.elsner@hotmail.com>
2025-01-02 13:21:06 -0300

    delete app controller and app service

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 
 @Module({
   imports: [OrdersModule, RabbitmqModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
