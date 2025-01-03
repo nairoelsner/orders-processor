fda796218be34ba0f7fa4b61c5d59693a2fd8c92
Nairo Elsner <nairo.elsner@hotmail.com>
2025-01-03 01:39:45 -0300

    add dockerfiles

--- nestjs-api/src/main.ts
+++ nestjs-api/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log('Starting application');
+  console.log('RABBITMQ_URI --->', process.env.RABBITMQ_URI);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- nestjs-api/src/rabbitmq/rabbitmq.module.ts
+++ nestjs-api/src/rabbitmq/rabbitmq.module.ts
@@ -2,18 +2,18 @@ import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [
-        RabbitMQModule.forRoot(RabbitMQModule, {
-          exchanges: [
-            {
-              name: 'amq.topic',
-              type: 'topic',
-            },
-          ],
-          uri: 'amqp://guest:guest@localhost:5672',
-          connectionInitOptions: { wait: false },
-        }),
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'amq.topic',
+          type: 'topic',
+        },
       ],
-      exports: [RabbitMQModule],
+      uri: process.env.RABBITMQ_URI || 'amqp://guest:guest@localhost:5672',
+      connectionInitOptions: { wait: false },
+    }),
+  ],
+  exports: [RabbitMQModule],
 })
 export class RabbitmqModule {}
